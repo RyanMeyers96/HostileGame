239e363eaf222b60bbdf057e375ed39f78210313
Michael <1013532@student.sae.edu.au>
2019-02-26 16:16:47 +1100

    MB More stuff on scripts
    more in depth states and basic movement for player

---
+++ Mors Antrum/Assets/Scripts/GrandMeep.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrandMeep : MonoBehaviour
+{
+
+    public delegate void SummonLight(GameObject Light);
+    public event SummonLight OnSummonedLight;
+    public event SummonLight OnDeSummonLight;
+    [SerializeField] private float speedMult;
+    [SerializeField]
+    private Camera mainCamera;
+    [SerializeField]
+    private GameObject lightPrefab;
+    private GameObject light;
+
+    private void Start()
+    {
+        mainCamera = FindObjectOfType<Camera>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                light = Instantiate(lightPrefab, new Vector3(hit.point.x, hit.point.y, 1), new Quaternion(0, 0, 0, 0));
+                
+            }           
+            if (light) OnSummonedLight(light);
+        }
+        if (Input.GetMouseButtonUp(0)) OnDeSummonLight(light);
+        transform.Translate(Input.GetAxis("Horizontal")/speedMult, Input.GetAxis("Vertical")/speedMult,0);
+    }
+}
