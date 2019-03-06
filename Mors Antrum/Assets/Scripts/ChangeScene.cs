66d2303296ebf7827587bad9f5016d3a0b3b93ca
Celina <koroji_hun@hotmail.com>
2019-03-06 23:37:20 +1100

    Change scene script

---
+++ Mors Antrum/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ChangeScene : MonoBehaviour
+{
+    public string NextLevel; //Type the correct scene name in the Inspector
+
+
+    //When players collided with gameobject with script
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) 
+        {
+            SceneManager.LoadScene(NextLevel); //Load into scene
+
+        }
+
+
+    }
+}
