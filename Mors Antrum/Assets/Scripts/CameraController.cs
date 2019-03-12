970c354dcd0084bbc9800e87e65bae83eaf6c8ef
Ryan <1013168@student.sae.edu.au>
2019-03-12 16:54:54 +1100

    RM Adjusted camera, implement testing feedback

--- Mors Antrum/Assets/Scripts/CameraController.cs
+++ Mors Antrum/Assets/Scripts/CameraController.cs
@@ -6,11 +6,21 @@ public class CameraController : MonoBehaviour
 {
         
 	public Transform target;  //the player
-	public Vector3 offset = new Vector3(0f,0f,-19f);  //how far from them the player the camera is
+	public Vector3 offset = new Vector3(0f,0f,-8.5f);  //how far from them the player the camera is
 
 	public float boundX; //used to clamp the x variable
 	public float boundY; //used to clamp the y variable
-	
+
+    public GameObject ceiling;
+    public GameObject floor;
+    public GameObject leftWall;
+    public GameObject rightWall;
+
+    public float leftWallOffset;
+    public float rightWallOffset;
+    public float ceilingOffset;
+    public float floorOffset;
+
 	public float smoothSpeed = 0.125f;  //used to make the camera movement speed
 
 
@@ -20,8 +30,8 @@ public class CameraController : MonoBehaviour
 		boundX = target.position.x;
 		boundY = target.position.y;
 		
-		boundX = (Mathf.Clamp(boundX, -0.4f, .4f));
-		boundY = (Mathf.Clamp(boundY, 0.7f, 28f));
+		boundX = (Mathf.Clamp(boundX, (leftWall.transform.position.x + leftWallOffset), (rightWall.transform.position.x- rightWallOffset)));
+		boundY = (Mathf.Clamp(boundY, (floor.transform.position.y + floorOffset), (ceiling.transform.position.y - ceilingOffset)));
 		
 		//set the desiredPosition - this is where the camera wants to be, then sends the smoothPosition - this makes the camera move smoothly towards the position
 		Vector3 desiredPosition = new Vector3(boundX, boundY, target.position.z) + offset;
