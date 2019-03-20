939cbfd300df0a9e52b1e0a48c4a9f06d757e0a3
Venus <inkyinked@gmail.com>
2019-03-21 01:19:03 +1100

    Pushing the moving platforms

---
+++ Mors Antrum/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public Transform[] Waypoints;
+    public float speed = 2;
+
+    public int CurrentPoint = 0;
+
+    //private IEnumerator coroutine;
+    public float DelayTime;
+    public float CurrentTimer;
+
+    void Start()
+    {
+        CurrentTimer = DelayTime;
+    }
+    void Update()
+    {
+        if (transform.position.y != Waypoints[CurrentPoint].transform.position.y)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Waypoints[CurrentPoint].transform.position, speed * Time.deltaTime);
+        }
+
+        if (transform.position.y == Waypoints[CurrentPoint].transform.position.y)
+        {
+            //StartCoroutine("Wait");
+            Delay();
+        }
+        if (CurrentPoint >= Waypoints.Length)
+        {
+            CurrentPoint = 0;
+        }
+    }
+
+    private void Delay()
+    {
+        
+        CurrentTimer -= Time.deltaTime;
+        if (CurrentTimer <=0)
+        {
+            CurrentPoint += 1;
+            CurrentTimer = DelayTime;
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, transform.position, speed * Time.deltaTime);   // Probably better way
+        }
+    }
+}
