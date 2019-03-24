7c0428f546e406da799cad6729c6c5922b33d73e
Celina <koroji_hun@hotmail.com>
2019-03-24 22:52:28 +1100

    scene Level 1
    add tutorial script, still working on it.
    add UI canvas, add image testing

---
+++ Mors Antrum/Assets/Scripts/TutorialTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   /* void OnTriggerEnter(collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+           
+           //collide with player with tutorial
+            //tick off once player success WASD
+            //how to make tutorial disappear after compeleted
+        }
+    }
+    */
+}
