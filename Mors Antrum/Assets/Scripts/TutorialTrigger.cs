0c8e1a04c347e815e55eab41ff2b73a6caf1b189
Celina <koroji_hun@hotmail.com>
2019-03-25 11:47:18 +1100

    Working on TriggerTutorial Script
    script testing

--- Mors Antrum/Assets/Scripts/TutorialTrigger.cs
+++ Mors Antrum/Assets/Scripts/TutorialTrigger.cs
@@ -5,6 +5,9 @@ using UnityEngine.UI;
 
 public class TutorialTrigger : MonoBehaviour
 {
+    //[SerializeField] private Image customImage;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,15 +20,38 @@ public class TutorialTrigger : MonoBehaviour
         
     }
 
-   /* void OnTriggerEnter(collider other)
-    {
+    /* void OnTriggerEnter(collider other) //collide with player with tutorial
+     {
+         if (other.CompareTag("Player")) //Image appear to player when tag
+         {
+            Instantiate(prefab, new Vector3 (x, y,0), Quaternion.identity); 
+            Destroy(GameObject);                    //how to make tutorial disappear after compeleted
+
+
+
+         }
+ 
+     }
+
+    void OnTriggerEnter(collider other)
+    { 
         if (other.CompareTag("Player"))
         {
-           
-           //collide with player with tutorial
-            //tick off once player success WASD
-            //how to make tutorial disappear after compeleted
+            customImage.enable = true;
         }
+    
     }
-    */
+    
+    void OnTriggerExit(collider other)
+    { 
+        if (other.CompareTag("Player"))
+        {
+            customImage.enable = false;
+        }
+    
+    }
+
+
+
+     */
 }
