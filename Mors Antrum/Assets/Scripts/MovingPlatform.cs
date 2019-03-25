a9385bf47786fae882dc32bb4d7d9ec038c4f2b1
Ryan <1013168@student.sae.edu.au>
2019-03-25 11:29:12 +1100

    RM Implemented moving platform scripts

--- Mors Antrum/Assets/Scripts/MovingPlatform.cs
+++ Mors Antrum/Assets/Scripts/MovingPlatform.cs
@@ -28,7 +28,10 @@ public class MovingPlatform : MonoBehaviour
         {
             //StartCoroutine("Wait");
             Delay();
+            
         }
+
+
         if (CurrentPoint >= Waypoints.Length)
         {
             CurrentPoint = 0;
