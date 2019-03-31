864611ee522bab4ad8bd2648ae76b17a4d2b6db7
Celina <koroji_hun@hotmail.com>
2019-03-31 23:42:35 +1100

    Scene level 1
    - added replacement PNG image
    - added blockplayer script (code refuse to work)

---
+++ Mors Antrum/Assets/Scripts/BlockPlayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockPlayer : MonoBehaviour
+{
+    //public GameObject Wall;
+
+
+    void OnColliionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Player")) //wanted only the player to touch
+        {
+
+            if (col.gameObject.name == "block") //Stupid code won't work, it worked on everyone else but not this one
+            {
+                Debug.Log("Destroyed");
+                Vanish();                       //trying to destroy test then timer added
+
+            }
+
+        }
+    }
+
+    void Vanish ()
+    {
+        Destroy(this.gameObject);
+
+    }
+
+
+}
+
+
+   
+
