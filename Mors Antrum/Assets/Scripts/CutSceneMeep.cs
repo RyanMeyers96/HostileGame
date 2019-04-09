ad18503bba29955dccdfdae4fbbbe2ae5d433b24
Hayley <hayley@moxiehalo.games>
2019-04-09 16:26:02 +1000

    End Scene
    Assets in

---
+++ Mors Antrum/Assets/Scripts/CutSceneMeep.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutSceneMeep : MonoBehaviour
+{
+    public Transform[] Waypoints;
+    public float speed = 2f;
+
+    public int CurrentPoint = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position != Waypoints[CurrentPoint].transform.position)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Waypoints[CurrentPoint].transform.position, speed * Time.deltaTime);
+
+        }
+
+        if (CurrentPoint >= Waypoints.Length)
+        {
+
+            CurrentPoint = 0;
+        }
+    }
+}
