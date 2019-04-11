b1faec4c92e0d14f0542657112d8c7406ad5c129
boredomsMuse <1011084@student.sae.edu.au>
2019-04-11 16:29:40 +1000

    Nope moving

--- Mors Antrum/Assets/Scripts/MeepEating.cs
+++ Mors Antrum/Assets/Scripts/MeepEating.cs
@@ -1,19 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class MeepEating : MonoBehaviour
 {
-
+    
     private GameObject objTrigger;
     private MeepBehavior eatMeep;
-    //public List<MeepBehavior> meepsToBeDead;  - no longer needed
     private GrandMeep eatPlayer;
     [SerializeField] private GameObject mouth;
 
     public int meepsWanted = 1; //how many meeps the nope wants to eat
     public int meepsHad;//how many meeps the nope has eaten
-    
+
+    //animation related
+    public bool eat;
+    public bool stop;
     
     private void OnTriggerEnter(Collider other)
     {
@@ -25,59 +28,32 @@ public class MeepEating : MonoBehaviour
         
         if (eatMeep != null)
         {
-            //make meeps jump
+            eat = true;
             meepsHad++;
             GameObject.Find("MeepCount").GetComponent<MeepCountScript>().meepCount++;
             eatMeep.myState = eatMeep.dead;
             eatMeep.nope = mouth;
-            //meepsToBeDead.Add(objTrigger.GetComponent<MeepBehavior>()); - no longer useful, now that each meep is being eaten one at a time
+            eat = false;
         }
 
         if (eatPlayer != null)
         {
-            //the player is eaten by the meep
+            eat = true;
+            //eat player
+            eat = false;
         }
     }
 
-    /*  No longer useful now that the meeps are being eaten one at a time
-    private void OnTriggerExit(Collider other)
-    {
-        //everytime someone exits its mouth it checks if it is a meep, if yes meepsHad goes down
-        
-        objTrigger = other.gameObject;
-        
-        if (objTrigger.GetComponent<MeepBehavior>() != null)
-        {
-            meepsHad--;
-            meepsToBeDead.Remove(objTrigger.GetComponent<MeepBehavior>());
-        }
-    }*/
+   
 
     private void Update()
     {
-        /*check if the nope can eat now - no longer needed
         if (meepsHad == meepsWanted)
         {
-            //insert animation for eating
-            for (int i = 0; i <= meepsWanted; i++)
-            {
-                meepsToBeDead[i].myState = meepsToBeDead[i].dead;
-            }
-
-            Leave();
-        }*/
-
-        if (meepsHad == meepsWanted)
-        {
-            Leave();
+            stop = true;
         }
+
     }
 
-    private void Leave()
-    {
-        //animation for leaving
-        //more transform backwards
-        //delete self once done
-    }
 }
 
